7ae759a201924b7e3636ec0d3f237e6ff623c845
Nicholas Chee-Awai <cheeordie1@gmail.com>
2016-03-03 04:18:19 -0800

    added indices and normalized primary data set

--- proj.R
+++ proj.R
@@ -11,9 +11,34 @@ require(glmnet)
 # security_data <- read.table("XBD_SECURITY_DEA.csv", sep=",", header=TRUE, as.is = TRUE, quote="\"")
 
 # Response Variable: Tech Index
-# Correlate S&P, Chinese Market, Japanese Market, tax laws, interest rates, ...
-s_p_data <- read.table("S&P.csv", sep=",", header=TRUE, as.is = TRUE, quote="\"")
 tech_data <- read.table("XCI_TECH_IDX.csv", sep=",", header=TRUE, as.is = TRUE, quote="\"")
+# Correlate S&P, Shanghai Market, Hong Kong Market, Japanese Market, tax laws, interest rates, ...
+s_p_data <- read.table("S&P.csv", sep=",", header=TRUE, as.is = TRUE, quote="\"")
+nikkei_data <- read.table("NIKKEI.csv", sep=",", header=TRUE, as.is = TRUE, quote="\"")
+shanghai_data <- read.table("SHANGHAI.csv", sep=",", header=TRUE, as.is = TRUE, quote="\"")
+hong_kong_data <- read.table("HONG_KONG.csv", sep=",", header=TRUE, as.is = TRUE, quote="\"")
 
+# Calculate Weekly % change
+# Weekly % Change = (This week's price - last Week's price) / (last week's price * 100)
+p_change <- function(d_frame) {
+  # sort table by date
+  d_frame <- arrange(d_frame, desc(format(Date, format="%Y-%B-%d")))
+  d_frame$wp_change <- ((d_frame$Close - d_frame$Close[2:(nrow(d_frame)+1)]) / d_frame$Close[2:(nrow(d_frame)+1)])* 100
+  d_frame[is.na(d_frame)] <- 0
+  return(d_frame)
+}
+
+tech_data <- p_change(tech_data)
+s_p_data <- p_change(s_p_data)
+nikkei_data <- p_change(nikkei_data)
+shanghai_data <- p_change(shanghai_data)
+hong_kong_data <- p_change(hong_kong_data)
 
 # normalize tables
+primary_data <- select(inner_join(tech_data, s_p_data, by="Date"), Date, tech_wp_change=wp_change.x, sp_wp_change=wp_change.y)
+primary_data <- select(inner_join(primary_data, nikkei_data, by="Date"), Date, tech_wp_change, sp_wp_change, nikkei_wp_change=wp_change)
+primary_data <- select(inner_join(primary_data, shanghai_data, by="Date"), Date, tech_wp_change, sp_wp_change, nikkei_wp_change,shanghai_wp_change=wp_change)
+primary_data <- select(inner_join(primary_data, hong_kong_data, by="Date"), Date, tech_wp_change, sp_wp_change, nikkei_wp_change,shanghai_wp_change, hong_kong_wp_change=wp_change)
+
+# Beta(stock) = covariance(Stock's % weekly change, index's % weekly change) / Variance(index's % weekly change)
+
